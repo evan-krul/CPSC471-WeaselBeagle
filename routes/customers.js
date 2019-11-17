71f0a257522024d6a4197a2dc38e07492e3099a3
evan-krul <evan@krul.ca>
2019-11-16 21:19:04 -0700

    Backend fixes

--- routes/customers.js
+++ routes/customers.js
@@ -1,133 +1,120 @@
+module.exports = function (app) {
+    app.get('/customers', function (req, res) {
+        req.getConnection(function (err, connection) {
 
-/*
- * GET users listing.
- */
+            var query = connection.query('SELECT * FROM customer', function (err, rows) {
 
-exports.list = function(req, res){
+                if (err)
+                    console.log("Error Selecting : %s ", err);
+                //console.log(rows);
+                //res.render('customers',{page_title:"Customers - Node.js",data:rows});
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify(rows));
+                //res.end(JSON.stringify(rows));
 
-  req.getConnection(function(err,connection){
-       
-        var query = connection.query('SELECT * FROM customer',function(err,rows)
-        {
-            
-            if(err)
-                console.log("Error Selecting : %s ",err );
-            //console.log(rows);
-            //res.render('customers',{page_title:"Customers - Node.js",data:rows});
-            res.setHeader('Content-Type', 'application/json');
-            res.send(JSON.stringify(rows));
-            //res.end(JSON.stringify(rows));  
-           
-         });
-         
-         //console.log(query.sql);
-    });
-  
-};
-
-exports.edit = function(req, res){
-    
-    var id = req.params.id;
-    
-    req.getConnection(function(err,connection){
-       
-        var query = connection.query('SELECT * FROM customer WHERE id = ?',[id],function(err,rows)
-        {
-            
-            if(err)
-                console.log("Error Selecting : %s ",err );
-     
-           // res.render('edit_customer',{page_title:"Edit Customers - Node.js",data:rows});
-            res.setHeader('Content-Type', 'application/json');
-            res.send(JSON.stringify(rows));   
-           
-         });
-         
-         //console.log(query.sql);
-    }); 
-};
-
-/*Save the customer*/
-exports.save = function(req,res){
-    
-    var input = JSON.parse(JSON.stringify(req.body));
-    
-    req.getConnection(function (err, connection) {
-        
-        var data = {
-            
-            name    : input.name,
-            address : input.address,
-            email   : input.email,
-            phone   : input.mobile 
-        
-        };
-       console.log('save request...', data);
-
-        var query = connection.query("INSERT INTO customer set ? ",data, function(err, rows)
-        {
-  
-          if (err)
-              console.log("Error inserting : %s ",err );
-         
-          //res.redirect('/customers');
-            res.setHeader('Content-Type', 'application/json');
-            res.send(JSON.stringify('success'));
-          
+            });
+            //console.log(query.sql);
         });
-        
-       // console.log(query.sql); get raw query
-    
+    });
+
+
+    app.get('/customers/add', function (req, res) {
+
+        var input = JSON.parse(JSON.stringify(req.body));
+
+        req.getConnection(function (err, connection) {
+
+            var data = {
+
+                name: input.name,
+                address: input.address,
+                email: input.email,
+                phone: input.mobile
+
+            };
+            console.log('save request...', data);
+
+            var query = connection.query("INSERT INTO customer set ? ", data, function (err, rows) {
+
+                if (err)
+                    console.log("Error inserting : %s ", err);
+
+                //res.redirect('/customers');
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify('success'));
+
+            });
+
+            // console.log(query.sql); get raw query
+
+        });
+    });
+
+    app.get('/customers/delete/:id', function (req, res) {
+
+        var id = req.params.id;
+
+        req.getConnection(function (err, connection) {
+
+            connection.query("DELETE FROM customer  WHERE id = ? ", [id], function (err, rows) {
+
+                if (err)
+                    console.log("Error deleting : %s ", err);
+
+                res.redirect('/customers');
+
+            });
+
+        });
+    });
+
+
+    app.get('/customers/edit/:id', function (req, res) {
+
+        var id = req.params.id;
+
+        req.getConnection(function (err, connection) {
+
+            var query = connection.query('SELECT * FROM customer WHERE id = ?', [id], function (err, rows) {
+
+                if (err)
+                    console.log("Error Selecting : %s ", err);
+
+                // res.render('edit_customer',{page_title:"Edit Customers - Node.js",data:rows});
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify(rows));
+
+            });
+
+            //console.log(query.sql);
+        });
+    });
+
+    app.post('/customers/edit/:id', function (req, res) {
+
+        var input = JSON.parse(JSON.stringify(req.body));
+        var id = req.params.id;
+
+        req.getConnection(function (err, connection) {
+
+            var data = {
+
+                name: input.name,
+                address: input.address,
+                email: input.email,
+                phone: input.phone
+
+            };
+
+            connection.query("UPDATE customer set ? WHERE id = ? ", [data, id], function (err, rows) {
+
+                if (err)
+                    console.log("Error Updating : %s ", err);
+
+                res.redirect('/customers');
+
+            });
+
+        });
     });
 };
-
-exports.save_edit = function(req,res){
-    
-    var input = JSON.parse(JSON.stringify(req.body));
-    var id = req.params.id;
-    
-    req.getConnection(function (err, connection) {
-        
-        var data = {
-            
-            name    : input.name,
-            address : input.address,
-            email   : input.email,
-            phone   : input.phone 
-        
-        };
-        
-        connection.query("UPDATE customer set ? WHERE id = ? ",[data,id], function(err, rows)
-        {
-  
-          if (err)
-              console.log("Error Updating : %s ",err );
-         
-          res.redirect('/customers');
-          
-        });
-    
-    });
-};
-
-
-exports.delete_customer = function(req,res){
-          
-     var id = req.params.id;
-    
-     req.getConnection(function (err, connection) {
-        
-        connection.query("DELETE FROM customer  WHERE id = ? ",[id], function(err, rows)
-        {
-            
-             if(err)
-                 console.log("Error deleting : %s ",err );
-            
-             res.redirect('/customers');
-             
-        });
-        
-     });
-};
-
-
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-
 /**
  * Module dependencies.
  */
+
 
 var express = require('express');
 //var routes = require('./routes');
@@ -10,17 +10,13 @@ var path = require('path');
 var fs = require("fs");
 
 //load customers route
-var customers = require('./routes/customers'); 
 var app = express();
 
-var connection  = require('express-myconnection'); 
+var connection  = require('express-myconnection');
 var mysql = require('mysql');
 
 // all environments
 app.set('port', process.env.PORT || 4300);
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'ejs');
-//app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
@@ -39,9 +35,9 @@ if ('development' == app.get('env')) {
 -------------------------------------------*/
 
 app.use(
-    
+
     connection(mysql,{
-        
+
         host: '34.94.217.52', //'localhost',
         user: 'wb',
         password : '&SRNxv5*KnkD',
@@ -52,6 +48,7 @@ app.use(
 
 );
 
+require('./routes')(app);
 
 /*
  * GET home page.
@@ -60,13 +57,6 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + './index.html'));
 });
 
-//app.get('/', routes.index);
-app.get('/customers', customers.list);
-//app.get('/customers/add', customers.add);
-app.post('/customers/add', customers.save);
-app.get('/customers/delete/:id', customers.delete_customer);
-app.get('/customers/edit/:id', customers.edit);
-app.post('/customers/edit/:id',customers.save_edit);
 
 
 //app.use(app.router);
