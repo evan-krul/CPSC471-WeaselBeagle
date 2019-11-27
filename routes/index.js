56eba6b375ffef5b732671c79a2d6265c0d6aa3a
Layla Arab <layla.arab@icloud.com>
2019-11-27 12:34:58 -0700

    Fixed formatting

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 
-module.exports = function(app){
-    fs.readdirSync(__dirname).forEach(function(file) {
-        if (file === "index.js") return;
-        const name = file.substr(0, file.indexOf('.'));
-        console.log(name);
-        require('./' + name)(app);
-    });
+module.exports = function (app) {
+  fs.readdirSync(__dirname).forEach(function (file) {
+    if (file === "index.js") return;
+    const name = file.substr(0, file.indexOf('.'));
+    console.log(name);
+    require('./' + name)(app);
+  });
 };
