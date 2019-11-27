3c207006b2a3c0e614511075e679ecaa13474418
Layla Arab <layla.arab@icloud.com>
2019-11-27 12:43:01 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ module.exports = function (app) {
   fs.readdirSync(__dirname).forEach(function (file) {
     if (file === "index.js") return;
     const name = file.substr(0, file.indexOf('.'));
-    console.log(name);
     require('./' + name)(app);
   });
 };
