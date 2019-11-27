1ff1f9c12cb0a6ac5ea5094b27f81e69b4d13ea7
Layla Arab <layla.arab@icloud.com>
2019-11-27 12:35:57 -0700

    Formatting v2

--- routes/AdminAPI.js
+++ routes/AdminAPI.js
@@ -1,107 +1,108 @@
 const {phoneNumbersInputHandler} = require("../public/helpers/AccountHelpers");
 const {accountInputHandler} = require("../public/helpers/AccountHelpers");
 module.exports = function (app) {
-    app.get('/api/admin', function (req, res) {
-        req.getConnection(function (err, connection) {
-            connection.query('SELECT * FROM Account JOIN Trainer ON email = ?', [req.email], function (err, rows) {
-                if (err) {
-                    console.log("Error Selecting : %s ", err);
-                }
-                res.setHeader('Content-Type', 'application/json');
-                res.send(JSON.stringify(rows));
-            });
-        });
-    });
 
-    app.post('/api/admin/add', function (req, res) {
-        let input = JSON.parse(JSON.stringify(req.body));
-        req.getConnection(function (err, connection) {
-            let data = trainerInputHandler(input);
-            console.log('save request...', data);
-            connection.query("INSERT INTO Account set ? ", [data.account], function (err) {
-                if (err) {
-                    console.log("Error inserting : %s ", err);
-                }
-            });
-            connection.query("INSERT INTO Admin set ?", [data.admin], function (err) {
-                if (err) {
-                    console.log("Error inserting : %s ", err);
-                }
-            });
-            connection.query("INSERT INTO UserAccountPhoneNumbers set ? ", [data.userAccountPhoneNumbers], function (err) {
-                if (err) {
-                    console.log("Error inserting : %s ", err);
-                }
-            });
-            res.setHeader('Content-Type', 'application/json');
-            res.send(JSON.stringify('success'));
-        });
+  app.get('/api/admin', function (req, res) {
+    req.getConnection(function (err, connection) {
+      connection.query('SELECT * FROM Account JOIN Trainer ON email = ?', [req.email], function (err, rows) {
+        if (err) {
+          console.log("Error Selecting : %s ", err);
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(rows));
+      });
     });
+  });
 
-    app.get('/api/trainer/delete/:id', function (req, res) {
-        let email = req.params.email;
-        req.getConnection(function (err, connection) {
-            connection.query("DELETE FROM Account WHERE email = ?", [email], function (err) {
-                if (err) {
-                    console.log("Error deleting : %s ", err);
-                }
-                res.redirect('/api/trainer');
-            });
-        });
+  app.post('/api/admin/add', function (req, res) {
+    let input = JSON.parse(JSON.stringify(req.body));
+    req.getConnection(function (err, connection) {
+      let data = trainerInputHandler(input);
+      console.log('save request...', data);
+      connection.query("INSERT INTO Account set ? ", [data.account], function (err) {
+        if (err) {
+          console.log("Error inserting : %s ", err);
+        }
+      });
+      connection.query("INSERT INTO Admin set ?", [data.admin], function (err) {
+        if (err) {
+          console.log("Error inserting : %s ", err);
+        }
+      });
+      connection.query("INSERT INTO UserAccountPhoneNumbers set ? ", [data.userAccountPhoneNumbers], function (err) {
+        if (err) {
+          console.log("Error inserting : %s ", err);
+        }
+      });
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify('success'));
     });
+  });
 
-    app.get('/api/trainer/edit/:id', function (req, res) {
-        let email = req.params.email;
-        req.getConnection(function (err, connection) {
-            connection.query('SELECT * FROM Account JOIN Trainer ON email = ?', [email], function (err, rows) {
-                if (err) {
-                    console.log("Error Selecting : %s ", err);
-                }
-                res.setHeader('Content-Type', 'application/json');
-                res.send(JSON.stringify(rows));
-            });
-        });
+  app.get('/api/trainer/delete/:id', function (req, res) {
+    let email = req.params.email;
+    req.getConnection(function (err, connection) {
+      connection.query("DELETE FROM Account WHERE email = ?", [email], function (err) {
+        if (err) {
+          console.log("Error deleting : %s ", err);
+        }
+        res.redirect('/api/trainer');
+      });
     });
+  });
 
-    app.post('/api/trainer/edit/:id', function (req, res) {
-        let input = JSON.parse(JSON.stringify(req.body));
-        req.getConnection(function (err, connection) {
-            let data = trainerInputHandler(input);
-            console.log('save request...', data);
-            i
-            connection.query("INSERT INTO Account set ? ", [data.account], function (err) {
-                if (err) {
-                    console.log("Error inserting : %s ", err);
-                }
-            });
-            connection.query("INSERT INTO Trainer set ?", [data.trainer], function (err) {
-                if (err) {
-                    console.log("Error inserting : %s ", err);
-                }
-            });
-            connection.query("INSERT INTO UserAccountPhoneNumbers set ? ", [data.userAccountPhoneNumbers], function (err) {
-                if (err) {
-                    console.log("Error inserting : %s ", err);
-                }
-            });
-            res.setHeader('Content-Type', 'application/json');
-            res.send(JSON.stringify(rows));
-        });
+  app.get('/api/trainer/edit/:id', function (req, res) {
+    let email = req.params.email;
+    req.getConnection(function (err, connection) {
+      connection.query('SELECT * FROM Account JOIN Trainer ON email = ?', [email], function (err, rows) {
+        if (err) {
+          console.log("Error Selecting : %s ", err);
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(rows));
+      });
     });
+  });
 
-    function trainerInputHandler(input) {
-        return {
-            account: {
-                accountVars: accountInputHandler(input),
-                accountType: "admin"
-            },
-            admin: {
-                email: input.email
-            },
-            userAccountPhoneNumbers: {
-                phoneNumbersInputHandler
-            },
-        };
-    }
+  app.post('/api/trainer/edit/:id', function (req, res) {
+    let input = JSON.parse(JSON.stringify(req.body));
+    req.getConnection(function (err, connection) {
+      let data = trainerInputHandler(input);
+      console.log('save request...', data);
+      i
+      connection.query("INSERT INTO Account set ? ", [data.account], function (err) {
+        if (err) {
+          console.log("Error inserting : %s ", err);
+        }
+      });
+      connection.query("INSERT INTO Trainer set ?", [data.trainer], function (err) {
+        if (err) {
+          console.log("Error inserting : %s ", err);
+        }
+      });
+      connection.query("INSERT INTO UserAccountPhoneNumbers set ? ", [data.userAccountPhoneNumbers], function (err) {
+        if (err) {
+          console.log("Error inserting : %s ", err);
+        }
+      });
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(rows));
+    });
+  });
+
+  function trainerInputHandler(input) {
+    return {
+      account: {
+        accountVars: accountInputHandler(input),
+        accountType: "admin"
+      },
+      admin: {
+        email: input.email
+      },
+      userAccountPhoneNumbers: {
+        phoneNumbersInputHandler
+      },
+    };
+  }
 
 }; // end of module
--- routes/AdopterAPI.js
+++ routes/AdopterAPI.js
@@ -1,109 +1,110 @@
 const {phoneNumbersInputHandler} = require("../public/helpers/AccountHelpers");
 const {accountInputHandler} = require("../public/helpers/AccountHelpers");
 module.exports = function (app) {
-    app.get('/api/adopter', function (req, res) {
-        req.getConnection(function (err, connection) {
-            connection.query('SELECT * FROM Account JOIN Adopter ON email = ?', [req.email], function (err, rows) {
-                if (err) {
-                    console.log("Error Selecting : %s ", err);
-                }
-                res.setHeader('Content-Type', 'application/json');
-                res.send(JSON.stringify(rows));
-            });
-        });
+
+  app.get('/api/adopter', function (req, res) {
+    req.getConnection(function (err, connection) {
+      connection.query('SELECT * FROM Account JOIN Adopter ON email = ?', [req.email], function (err, rows) {
+        if (err) {
+          console.log("Error Selecting : %s ", err);
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(rows));
+      });
+    });
+  });
+
+  app.post('/api/adopter/add', function (req, res) {
+    let input = JSON.parse(JSON.stringify(req.body));
+    req.getConnection(function (err, connection) {
+      let data = adopterInputHandler(input);
+      console.log('save request...', data);
+      connection.query("INSERT INTO Account set ?", [data.account], function (err) {
+        if (err) {
+          console.log("Error inserting : %s ", err);
+        }
+      });
+      connection.query("INSERT INTO Adopter set ?", [data.adopter], function (err) {
+        if (err) {
+          console.log("Error inserting : %s ", err);
+        }
+      });
+      connection.query("INSERT INTO UserAccountPhoneNumbers set ? ", [data.userAccountPhoneNumbers], function (err) {
+        if (err) {
+          console.log("Error inserting : %s ", err);
+        }
+      });
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify('success'));
     });
 
-    app.post('/api/adopter/add', function (req, res) {
-        let input = JSON.parse(JSON.stringify(req.body));
-        req.getConnection(function (err, connection) {
-            let data = adopterInputHandler(input);
-            console.log('save request...', data);
-            connection.query("INSERT INTO Account set ?", [data.account], function (err) {
-                if (err) {
-                    console.log("Error inserting : %s ", err);
-                }
-            });
-            connection.query("INSERT INTO Adopter set ?", [data.adopter], function (err) {
-                if (err) {
-                    console.log("Error inserting : %s ", err);
-                }
-            });
-            connection.query("INSERT INTO UserAccountPhoneNumbers set ? ", [data.userAccountPhoneNumbers], function (err) {
-                if (err) {
-                    console.log("Error inserting : %s ", err);
-                }
-            });
-            res.setHeader('Content-Type', 'application/json');
-            res.send(JSON.stringify('success'));
-        });
+  });
 
+  app.get('/api/adopter/delete/:id', function (req, res) {
+    let email = req.params.email;
+    req.getConnection(function (err, connection) {
+      connection.query("DELETE FROM Account WHERE email = ? ", [email], function (err) {
+        if (err) {
+          console.log("Error deleting : %s ", err);
+        }
+        res.redirect('/api/adopter');
+      });
     });
+  });
 
-    app.get('/api/adopter/delete/:id', function (req, res) {
-        let email = req.params.email;
-        req.getConnection(function (err, connection) {
-            connection.query("DELETE FROM Account WHERE email = ? ", [email], function (err) {
-                if (err) {
-                    console.log("Error deleting : %s ", err);
-                }
-                res.redirect('/api/adopter');
-            });
-        });
+  app.get('/api/adopter/edit/:id', function (req, res) {
+    let email = req.params.id;
+    req.getConnection(function (err, connection) {
+      connection.query('SELECT * FROM Account WHERE email = ?', [email], function (err, rows) {
+        if (err) {
+          console.log("Error Selecting : %s ", err);
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(rows));
+      });
     });
+  });
 
-    app.get('/api/adopter/edit/:id', function (req, res) {
-        let email = req.params.id;
-        req.getConnection(function (err, connection) {
-            connection.query('SELECT * FROM Account WHERE email = ?', [email], function (err, rows) {
-                if (err) {
-                    console.log("Error Selecting : %s ", err);
-                }
-                res.setHeader('Content-Type', 'application/json');
-                res.send(JSON.stringify(rows));
-            });
-        });
+  app.post('/api/adopter/edit/:id', function (req, res) {
+    let input = JSON.parse(JSON.stringify(req.body));
+    req.getConnection(function (err, connection) {
+      let data = adopterInputHandler(input);
+      console.log('save request...', data);
+      connection.query("INSERT INTO Account set ? ", [data.account], function (err) {
+        if (err) {
+          console.log("Error inserting : %s ", err);
+        }
+      });
+      connection.query("INSERT INTO Adopter set ?", [data.adopter], function (err) {
+        if (err) {
+          console.log("Error inserting : %s ", err);
+        }
+      });
+      connection.query("INSERT INTO UserAccountPhoneNumbers set ? ", [data.userAccountPhoneNumbers], function (err) {
+        if (err) {
+          console.log("Error inserting : %s ", err);
+        }
+      });
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(rows));
     });
+  });
 
-    app.post('/api/adopter/edit/:id', function (req, res) {
-        let input = JSON.parse(JSON.stringify(req.body));
-        req.getConnection(function (err, connection) {
-            let data = adopterInputHandler(input);
-            console.log('save request...', data);
-            connection.query("INSERT INTO Account set ? ", [data.account], function (err) {
-                if (err) {
-                    console.log("Error inserting : %s ", err);
-                }
-            });
-            connection.query("INSERT INTO Adopter set ?", [data.adopter], function (err) {
-                if (err) {
-                    console.log("Error inserting : %s ", err);
-                }
-            });
-            connection.query("INSERT INTO UserAccountPhoneNumbers set ? ", [data.userAccountPhoneNumbers], function (err) {
-                if (err) {
-                    console.log("Error inserting : %s ", err);
-                }
-            });
-            res.setHeader('Content-Type', 'application/json');
-            res.send(JSON.stringify(rows));
-        });
-    });
-
-    function adopterInputHandler(input) {
-        return {
-            account: {
-                accountVars: accountInputHandler(input),
-                accountType: "adopter"
-            },
-            userAccountPhoneNumbers: {
-                phoneNumbersInputHandler
-            },
-            trainer: {
-                scheduleID: input.scheduleID,
-                email: input.email
-            }
-        };
-    }
+  function adopterInputHandler(input) {
+    return {
+      account: {
+        accountVars: accountInputHandler(input),
+        accountType: "adopter"
+      },
+      userAccountPhoneNumbers: {
+        phoneNumbersInputHandler
+      },
+      trainer: {
+        scheduleID: input.scheduleID,
+        email: input.email
+      }
+    };
+  }
 
 
 };
