de283ce3f8c3fd0b757018c74886d6018be9bfe8
Layla Arab <layla.arab@icloud.com>
2019-12-05 15:51:59 -0700

    Finished views

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {SlimLoadingBarService} from 'ng2-slim-loading-bar';
-import { NavigationCancel,
+import {
+  NavigationCancel,
   Event,
   NavigationEnd,
   NavigationError,
   NavigationStart,
-  Router } from '@angular/router';
+  Router
+} from '@angular/router';
 import {AuthenticationService} from './services';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,10 +18,19 @@ import {AuthenticationService} from './services';
 export class AppComponent {
   title = 'frontEnd';
   loggedIn = this.authenticationService.isLoggedIn();
+  private account;
+  private accountType = 'not_logged_in';
+
   constructor(private loadingBar: SlimLoadingBarService, private router: Router, private authenticationService: AuthenticationService) {
     this.router.events.subscribe((event: Event) => {
       this.navigationInterceptor(event);
     });
+    const currentUser = this.authenticationService.currentUserValue;
+    if (currentUser) {
+      this.account = JSON.parse(localStorage.getItem('currentUser'));
+      this.accountType = this.account.accountType;
+      console.log(this.account);
+    }
   }
 
   logout() {
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {AuthenticationService} from '../services';
 
 @Component({
   selector: 'app-home',
@@ -7,13 +9,26 @@ import {Router} from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  private account;
 
   constructor(
+    private httpClient: HttpClient,
     private router: Router,
+    private authenticationService: AuthenticationService
   ) {
+    const currentUser = this.authenticationService.currentUserValue;
+    if (currentUser) {
+      this.account = JSON.parse(localStorage.getItem('currentUser'));
+    }
   }
 
   ngOnInit() {
+    if (this.account.accountType === 'trainer') {
+      this.router.navigate(['trainer/appointments/animals']);
+    }
+    if (this.account.accountType === 'vet') {
+      this.router.navigate(['vet/appointments/animals']);
+    }
   }
 
 }
